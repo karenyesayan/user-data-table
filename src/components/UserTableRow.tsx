0c381af444389cc173583f4960a303ca515bc129
karenyesayan <111521212+karenyesayan@users.noreply.github.com>
2025-03-28 12:59:14 +0400

    Update UserTableRow.tsx

--- src/components/UserTableRow.tsx
+++ src/components/UserTableRow.tsx
@@ -1,13 +1,6 @@
-import { useState, useEffect } from "react";
+import type { DataType } from "../lib/definitions";
 
-export default function UserTableRow({ id }: { id: number }) {
-  const [userData, setUserData] = useState({ phone: 0 });
-
-  useEffect(() => {
-    fetch(`https://dummyjson.com/users/${id}`)
-      .then((response) => response.json())
-      .then((data) => setUserData(data));
-  }, []);
-
-  return <p style={{ margin: 0 }}>{userData.phone}</p>;
+// @ts-ignore
+export default function UserTableRow({ phone, ...rest }: DataType) {
+  return <p style={{ margin: 0 }}>{phone}</p>;
 }
