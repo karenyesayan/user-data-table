59f40f143a80db62ffe7ba1f4fa116e73627e0ae
karenyesayan <111521212+karenyesayan@users.noreply.github.com>
2025-03-28 13:08:14 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
 import "@ant-design/v5-patch-for-react-19";
@@ -8,9 +7,7 @@ import App from "./App.tsx";
 import { store } from "./redux/store";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
